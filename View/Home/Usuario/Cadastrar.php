5f384cf6a540581f99630d50e52798c2ae1f8a11
Rafael Queiroz <rafael.castro6@fatec.sp.gov.br>
2017-08-16 10:54:04 -0300

    Melhorias no cadastro
    A tela de cadastro de usuário agora possuí os campos de cadastro de nome, sobrenome e telefone. Os campos estão com cores personalizadas através do arquivo css

--- View/Home/Usuario/Cadastrar.php
+++ View/Home/Usuario/Cadastrar.php
@@ -5,14 +5,43 @@
 	//Cadastro de usuários recebe um POSTBACK
 	if($_POST)
 	{
-		$tenis->nome = $_POST["nome"];
-		if($tenis->CadastrarProduto())
+		$usuario->nome = $_POST["nome"];
+		if($usuario->CadastrarUsuario())
 		{
-			$tenis->AvisoSucessoCadastro();
+			$usuario->AvisoSucessoCadastro();
 		}
 		else
 		{
-			$tenis->AvisoErroCadastro();
+			$usuario->AvisoErroCadastro();
 		}
 	}
 ?>
+
+			<div class="container">
+				<div class="row">
+					<form class="col s12 m12">
+						<div class="row">
+							<div class="input-field col s6 col m6">
+								<i class="material-icons prefix">account_circle</i>
+								<input id="nome" type="text" name="nome" class="validate" />
+								<label for="nome">Nome</label>
+							</div>
+							<div class="input-field col s6 col m6">
+								<input id="sobrenome" type="text" name="sobrenome" class="validate" />
+								<label for="nome">Sobrenome</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="input-field col s2 col m2">
+								<i class="material-icons prefix">phone</i>
+								<input id="ddd" type="text" name="ddd" placeholder="11" class="validate" />
+								<label for="ddd">ddd</label>
+							</div>
+							<div class="input-field col s2 col m10">
+								<input id="telefone" type="text" name="telefone" class="validate" />
+								<label for="telefone">Telefone</label>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
